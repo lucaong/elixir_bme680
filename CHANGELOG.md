389b99b97e0df772b869cd5052638fe8542cc822
Luca Ongaro <mail@lucaongaro.eu>
2020-02-02 16:55:13 +0100

    Version v0.2.0, supporting also BME280

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## v0.2.0
+
+  - Add support for BME280 (thanks to [@baldmountain](https://github.com/baldmountain))
+
 ## v0.1.4
 
   - Add configurable temperature compensation (`temperature_offset` option)
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirBme680.MixProject do
   def project do
     [
       app: :elixir_bme680,
-      version: "0.1.4",
+      version: "0.2.0",
       elixir: "~> 1.7",
       compilers: [:elixir_make] ++ Mix.compilers,
       aliases: aliases(),
